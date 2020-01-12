c4e21ac0fd1101116177d842cede138388d864b2
cheikh el moctar <elmoctar.yehdhih@gmail.com>
2020-01-12 12:10:36 +0100

    reposnive toggler workflow menu

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,12 @@
-$(document).on('click', 'li.parent', function () {
-  $(this).toggleClass('show');
-  $(this).toggleClass('collapsed collapse');
+
+$(document).ready(function () {
+
+  $(document).on('click', 'li.parent', function () {
+    $(this).toggleClass('show');
+    $(this).toggleClass('collapsed collapse');
+  });
+
+  $(document).on('click', '.collapse-icon .title', function () {
+    $('.wf-elements').slideToggle(200);
+  });
 });
