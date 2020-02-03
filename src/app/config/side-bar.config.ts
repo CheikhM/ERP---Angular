2e29c3c6af48dcea3946137d01433d7fddb74230
cheikh el moctar <elmoctar.yehdhih@gmail.com>
2020-02-04 00:55:10 +0100

    fix

--- src/app/config/side-bar.config.ts
+++ src/app/config/side-bar.config.ts
@@ -1,6 +1,7 @@
 
 export const menuElements = [
   {text: 'Dashboard', icon: 'icon-dashboard', route: '', parent: false, permission: '*'},
+  {text: 'Tasks', icon: 'tasks', route: '/tasks/all', parent: false, permission: 'CS'},
   {text: 'Projects', icon: 'icon-project', route: '/projects/all', parent: false, permission: 'PM'},
   {
     text: 'Sales', icon: 'icon-sales', route: null, parent: true, permission: 'SM', subelements: [
@@ -22,7 +23,6 @@ export const menuElements = [
     ]
   },
   {text: 'Warehouse', icon: 'warehouse-icon', route: '/warehouse/all', parent: false, permission: 'AC'},
-  {text: 'Tasks', icon: 'tasks', route: '/tasks/all', parent: false, permission: 'CS'},
   {text: 'Users', icon: 'users', route: '/users/all', parent: false, permission: 'CS'},
   {text: 'Backup', icon: 'backup-icon', route: '/backup', parent: false, permission: 'CS'},
 ];
